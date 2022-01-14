ef8111e07a5734e38655aa6c3add5a366a3a1ee7
Popa Catalin <63396963+annormally@users.noreply.github.com>
2022-01-14 15:35:10 +0200

    config models services app-module routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ListComponent} from "./components/list/list.component";
+import {DetailsComponent} from "./components/details/details.component";
+import {AddComponent} from "./components/add/add.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'tutorials', pathMatch: 'full'},
+  {path: 'tutorials', component: ListComponent},
+  {path: 'tutorials/:id', component: DetailsComponent},
+  {path: 'add', component: AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AddComponent } from './components/add/add.component';
-import { DetailsComponent } from './components/details/details.component';
-import { ListComponent } from './components/list/list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AddComponent} from './components/add/add.component';
+import {DetailsComponent} from './components/details/details.component';
+import {ListComponent} from './components/list/list.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { ListComponent } from './components/list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
