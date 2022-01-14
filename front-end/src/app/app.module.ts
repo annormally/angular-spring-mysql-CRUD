75882ba7526932438ae14eda7ba791aea7de5327
Popa Catalin <63396963+annormally@users.noreply.github.com>
2022-01-14 15:16:03 +0200

    services/components/models

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './components/add/add.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    DetailsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
